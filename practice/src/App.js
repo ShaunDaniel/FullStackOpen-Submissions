d5707d2f36752fef2edb64347fd96d55e359b5a3
s h a u n <shaun.daniel@science.christuniversity.in>
2023-03-22 16:12:34 +0530

    Part1.d Practice Code

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,13 +1,11 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [counter, SetCount] = useState(0)
+  const [clicks, SetCount] = useState({left:0,right:0})
   
-  const Display = ({counter}) =>{
-    return(
-      <h1>{counter}</h1>
-    )
-  }
+  const [allclicks, setAll] = useState([])
+
+  const[totalclicks,setTotal] = useState(0)
 
   const Button = ({text,handleClick}) =>{
     return(
@@ -19,20 +17,41 @@ const App = () => {
     
   }
 
+  
 
-  const IncrementNumber = () => SetCount(counter+1)
-  const DecrementNumber = () => SetCount(counter-1)
-  const ResetNumber = () => SetCount(0)
+  const LeftClick = () => {
+    const updatedleft = clicks.left + 1
+    SetCount({...clicks,left: updatedleft})
+    setAll(allclicks.concat("L"))
+    setTotal(updatedleft+clicks.right)
+  }
+  const RightClick = () => {
+    const updatedright = clicks.right + 1
+    SetCount({...clicks,left: clicks.left + 1})
+    setAll(allclicks.concat("R"))
+    setTotal(clicks.left+updatedright)
 
+  }
 
+console.log(allclicks)
   return (
     <>
-    <Display counter={counter}/>
-    <Button text = "+" handleClick = {IncrementNumber} />
-    <Button text = "Reset" handleClick = {ResetNumber} />
-    <Button text = "-" handleClick = {DecrementNumber} />
+    <div>
+      <h1>Right : {clicks.right}</h1>
+      <h1>Left : {clicks.left}</h1>
+      <h1>Recent Clicks: {allclicks.join(" ")}</h1>
+      <h1>Total Clicks: {totalclicks}</h1>
+
+    </div>
+    <Button text = "Right" handleClick = {RightClick} />
+    <Button text = "Left" handleClick = {LeftClick} />
+    
     </>
   )
 }
 
 export default App
+
+
+// Add total click count
+// Show all click using LRRL
