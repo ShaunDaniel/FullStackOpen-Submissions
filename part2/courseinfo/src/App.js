f0131877d71cda580123271be871cfb96a080044
s h a u n <73394707+ShaunDaniel@users.noreply.github.com>
2023-08-04 23:58:18 +0530

    2.2 & 2.3 : Course information step7&8
    Implemented calculation of total number of exercises using array.reduce

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -25,32 +25,42 @@ const App = () => {
     return <h1>{title}</h1>;
   };
 
-  const Part = ({part}) => {
+  const Part = ({ part }) => {
     return (
-      <li>{part.name} : {part.exercises}</li>
-    )
-  }
+      <li>
+        {part.name} : {part.exercises}
+      </li>
+    );
+  };
 
   const Content = ({ parts }) => {
     return (
       <>
         <ul>
-          {parts.map((part)=><Part part={part}/>)}
+          {parts.map((part) => (
+            <Part part={part} />
+          ))}
         </ul>
       </>
     );
-  }
+  };
+
+  const Total = ({ parts }) => {
+    const totalExercises = parts.reduce((total,part)=>total+part.exercises,0)
+    return <p>Total Exercises : {totalExercises}</p>
+  };
 
   const Course = (props) => {
     return (
       <>
         <Header title={props.course.name} />
         <Content parts={props.course.parts} />
+        <Total parts={props.course.parts} />
       </>
-    );
-  };
+    )
+  }
 
-  return <Course course={course} />;
-};
+  return <Course course={course} />
+}
 
 export default App;
