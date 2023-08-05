2294491ff3ca0bcebec5aa689399644badb07b15
s h a u n <73394707+ShaunDaniel@users.noreply.github.com>
2023-08-05 21:30:35 +0530

    2.6: The Phonebook Step1
    Add names only via useState

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { 
+      id:1,
+      name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState({id:0,name:''})
+
+  const handleName = (e) => {
+    setNewName({...newName,name:e.target.value})
+  }
+
+  const addPerson = (e) =>{
+    e.preventDefault()
+    setPersons(persons.concat({id:persons.length+1,name:newName.name}))
+  }
+
+  return (
+    <div>
+      <div>debug: {newName.name}</div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName.name} onChange={handleName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person)=><li key={person.id}>{person.name}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+export default App
