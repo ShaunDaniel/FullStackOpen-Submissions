7d6aded789f78be234b743a587e90c61037f1d6e
s h a u n <73394707+ShaunDaniel@users.noreply.github.com>
2023-08-05 22:20:22 +0530

    2.9*: The Phonebook Step4
    Added a search functionality

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,12 +6,38 @@ const App = () => {
       id:1,
       name: 'Arto Hellas',
       number: '9627391823'
-    }
-  ]) 
+    },
+    { 
+      id:2,
+      name: 'Shaun Daniel',
+      number: '8293847615'
+    },
+    { 
+      id:3,
+      name: 'Benny Daniel',
+      number: '7162534251'
+    },
+  ])
+
+  
   const [newName, setNewName] = useState({id:0,name:'',number:''})
+  const [filtered, setFiltered] = useState(false)
+  const [filter_query, setFilterQuery] = useState("")
+
+
+
+  const filteredArray = filtered ? persons.filter((person)=>person.name.toLowerCase().includes(filter_query.toLowerCase())) : persons
+
 
   const handleNumber = (e) =>{
     setNewName({...newName,number:e.target.value})
+
+  }
+
+
+  const handleFilter = (e) => {
+    setFilterQuery(e.target.value)
+    e.target.value="" ? setFiltered(false) : setFiltered(true) 
 
   }
 
@@ -36,11 +62,13 @@ const App = () => {
 
   }
 
+  
   return (
     <div>
       <div>debug: {newName.name} {newName.number}</div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
+        <h3>Add Contact</h3>
         <div>
           Name: <input value={newName.name} onChange={handleName}/>
           <br />
@@ -53,9 +81,12 @@ const App = () => {
           <button type="submit">Add Contact</button>
         </div>
       </form>
+      <br />
+      <h3>Search</h3>
+      <input type="text" value={filter_query} onChange={handleFilter}/>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person)=><li key={person.id}><b>{person.name}</b> - {person.number}</li>)}
+        {filteredArray.map((person)=><li key={person.id}><b>{person.name}</b> - {person.number}</li>)}
       </ul>
     </div>
   )
